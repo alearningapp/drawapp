d188b620a285d752c567cc30ec350681340929d6
test <test@test.com>
2024-10-04 20:18:20 +0800

    update

---
+++ src/ColorPicker.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const colors = [
+  'black', 'red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'gray'
+];
+
+const ColorPicker = ({ selectedColor, onChange }) => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', padding: '10px' }}>
+      {colors.map((color) => (
+        <div
+          key={color}
+          onClick={() => onChange(color)}
+          style={{
+            backgroundColor: color,
+            width: '40px',
+            height: '40px',
+            margin: '5px',
+            cursor: 'pointer',
+            border: selectedColor === color ? '2px solid black' : 'none',
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ColorPicker;
