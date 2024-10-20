bac741dc572a8dfba9be8f50ec23a660d73b29b7
test <test@test.com>
2024-10-20 16:06:30 +0800

    update

--- cmd.sh
+++ cmd.sh
@@ -11,11 +11,11 @@ GENERATE_SOURCEMAP=false BUILD_PATH=${dest}/${VERSION} PUBLIC_URL=./ npm run bui
 #(cd ${dest}  && git pull --rebase)
 
 mkdir -p ${dest}/${VERSION}
-if [[ $VERSION =~ ^t ]]; then
+if [[ ! $VERSION =~ ^t ]]; then
     sed -E -i  "s|<base href=\"[^\"]*?\">|<base href=\"/learning/${VERSION}/\">|" ${dest}/../index.html
 fi
 (cd ${dest} && git add . && git commit -am 'update' && while ! git push ; do echo 'lll';done;)
 if [[ $VERSION == t* ]]; then
-    echo "https://alearningapp.github.io/learning/${VERSION}/";
+    echo "https://alearningapp.com/learning/${VERSION}/";
 fi
 echo 'done';
