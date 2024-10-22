1c811825d23beeb7b24767670ff4df316f134135
test <test@test.com>
2024-10-22 22:15:46 +0800

    update

--- src/ModeSwitchButton.js
+++ src/ModeSwitchButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMusic, faPaintBrush, faVideo, faGamepad } from '@fortawesome/free-solid-svg-icons';
 
@@ -9,18 +9,17 @@ const ModeSwitchButton = () => {
     { icon: faVideo, name: 'Video' },
     { icon: faGamepad, name: 'Game' },
   ];
-  
-  const [currentModeIndex, setCurrentModeIndex] = useState(0);
-
-  const toggleMode = () => {
-    setCurrentModeIndex((prevIndex) => (prevIndex + 1) % modes.length);
-  };
 
   return (
     <div style={styles.container}>
-      <button onClick={toggleMode} style={styles.button}>
-        <FontAwesomeIcon icon={modes[currentModeIndex].icon} size="lg" />
-      </button>
+      {modes.map((mode, index) => (
+        <button key={index} style={styles.button}>
+          <div style={styles.iconContainer}>
+            <FontAwesomeIcon icon={mode.icon} size="sm" />
+            <span style={styles.label}>{mode.name}</span>
+          </div>
+        </button>
+      ))}
     </div>
   );
 };
@@ -36,7 +35,7 @@ const styles = {
   },
   button: {
     width: '40px',
-    height: '40px',
+    height: '60px', // Increased height to fit text below the icon
     cursor: 'pointer',
     border: 'none',
     borderRadius: '5px',
@@ -45,6 +44,18 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: '2px 0',
+    padding: '0',
+  },
+  iconContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  label: {
+    marginTop: '2px', // Space between icon and label
+    fontSize: '10px',
+    color: '#fff',
   },
 };
 
